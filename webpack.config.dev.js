cba2a8362a63ebe424b2d4509b887446dc12f1b4
Akanksha Singh <akanksha.singh@alumni.iiit.ac.in>
2020-07-28 23:43:00 +0530

    Moving babel code to package.json

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -49,28 +49,7 @@ export default {
         test: /\.(j|t)sx?$/,
         exclude: /node_modules/,
         include: path.join(__dirname, 'src'),
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            babelrc: false,
-            presets: [
-              [
-                '@babel/preset-env',
-                { targets: { browsers: 'last 2 versions' } }, // or whatever your project requires
-              ],
-              '@babel/preset-typescript',
-              '@babel/preset-react',
-            ],
-            plugins: [
-              // plugin-proposal-decorators is only needed if you're using experimental decorators in TypeScript
-              ['@babel/plugin-proposal-decorators', { legacy: true }],
-              ['@babel/plugin-proposal-class-properties', { loose: true }],
-              'react-hot-loader/babel',
-              '@babel/plugin-transform-runtime'
-            ],
-          },
-        },
+        use: ['babel-loader']
       },
       {
         test: /\.eot(\?v=\d+.\d+.\d+)?$/,
